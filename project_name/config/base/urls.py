23deac4ee45637d60fbf66d0dda8dea26dd47eaa
Serdar Tumgoren <zstumgoren@gmail.com>
2012-09-16 16:09:36 -0400

    add admin to base urls

--- project_name/config/base/urls.py
+++ project_name/config/base/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$', include('harmony.config.base.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
